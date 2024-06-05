7484f456a32b42a57f98d43bbc1b2a2ed66f1802
wang chuanbin <1418252648@qq.com>
2024-06-05 13:19:29 +0800

    测试

--- studygo/day01/03jinzhi/01/main.go
+++ studygo/day01/03jinzhi/01/main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
+	//十进制
+	var a int = 10
+	fmt.Printf("%d \n", a)
+	fmt.Printf("%b \n", a)
+
+	//八进制 以0开头
+	var b int = 077
+	fmt.Printf("%o \n", b)
 
 }

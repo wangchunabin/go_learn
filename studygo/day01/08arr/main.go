fe2d528460fd623ba352c74d3090f23bdd800389
wang chuanbin <1418252648@qq.com>
2024-07-01 14:47:24 +0800

    数组 二维数组

--- studygo/day01/08arr/main.go
+++ studygo/day01/08arr/main.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-func main() {
+func baseFunc() {
+	fmt.Println("===============基础=================")
 	//全局声明
 	var arr0 [5]int = [5]int{1, 2, 3, 4, 5}
 	var arr1 = [5]int{1, 2, 3, 4, 5}
@@ -17,6 +18,10 @@ func main() {
 		fmt.Println(arr0[i])
 	}
 
+	for index, value := range arr0 {
+		fmt.Printf("%d-%d\n", index, value)
+	}
+
 	//局部声明
 	a := [3]int{1, 2}               //未初始化元素值为0
 	b := [...]int{1, 2, 3, 4, 5, 6} //通过初始化值确定数组长度
@@ -24,4 +29,30 @@ func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+}
+
+func erweiArr() {
+	fmt.Println("===============二维数组=================")
+	a := [3][3]string{
+		{"北京", "上海"},
+		{"广州", "深圳"},
+		{"成都", "重庆"},
+	}
+	fmt.Println(a)
+	fmt.Println(a[2][1])
+
+	//二维数组循环
+	for _, v1 := range a {
+		for _, v2 := range v1 {
+			fmt.Printf("%s\t", v2)
+		}
+		fmt.Println()
+	}
+
+}
+
+func main() {
+	baseFunc()
+	erweiArr()
+
 }
